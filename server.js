12d5969162178c727b55d7cf2b869d07cd738e00
papjm <102404994+papjm@users.noreply.github.com>
2023-05-31 17:08:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response) {
-  console.log(request)
+app.get("/", function(req, res) {
+  res.send("<h1>My name is davinci</h1>")
 });
+
+app.get("/contact", function(req, res) {
+  res.send("My sahkajhska")
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000")
